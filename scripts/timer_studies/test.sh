fd252a69c916c3f86fe332d00fc87cf683afbb60
Vinicius <suporte.vinicius@garciawebsites.com.br>
2025-11-28 17:06:15 -0300

    Refatoramento

--- scripts/timer_studies/test.sh
+++ scripts/timer_studies/test.sh
@@ -2,7 +2,18 @@
 
 echo "======= Temporarizado de Estudos ======="
 
+kill_process(){
+    if [ "$(wc -c < /tmp/timer_pid)" -eq 0 ]; then
+        echo "Sem processo para parar!"
+        exit 0
+    fi
+
+    kill "$(cat /tmp/timer_pid)"
+    > /tmp/timer_pid
+}
+
 loop_time() {
+    # ps | grep test.sh | wc -l
     diff_acumulado=${1:-0}
     start=$(date +%s)
 
@@ -11,47 +22,61 @@ loop_time() {
         diff=$((now - start + diff_acumulado))
         printf "Tempo: %02d:%02d:%02d\n" \
             $((diff/3600)) $((diff%3600/60)) $((diff%60)) > /tmp/timer_log | sed -n '1p'
-
-        # salva infos (start atual, now atual, diff acumulado)
         echo "$start" > /tmp/times
         echo "$now"  >> /tmp/times
         echo "$diff" >> /tmp/times
-        echo "Temporarizador em Execução" >> /tmp/timer_log
+        echo "Status: Rodando" >> /tmp/timer_log
 
         sleep 1
     done
 }
 
-
-if [ "$1" = "start" ]; then
+start(){
     loop_time 0 &
     pid=$! 
     echo $pid > /tmp/timer_pid
-fi
+}
 
-if [ "$1" = "stop" ]; then 
-    diff=$(sed -n '3p' /tmp/times)
-    printf "Cronômetro: %02d:%02d:%02d\n" $((diff/3600)) $((diff%3600/60)) $((diff%60))
-    kill "$(cat /tmp/timer_pid)"
-    echo "" > /tmp/timer_pid
-fi
+stop(){
+    sed -i '1,2s/Rodando/Parado/' /tmp/timer_log 
+    kill_process
+}
 
-if [ "$1" = "continue" ]; then
+Continue(){
     start=$(sed -n '1p' /tmp/times)
-    diff_acomulado=$(sed -n '3p' /tmp/times)
-
+    diff_acomulado=$(awk 'NR==3' /tmp/times)
+    sed -i '2s/Parado/Rodando/' /tmp/timer_log
     loop_time "$diff_acomulado" &
     pid=$! 
     echo $pid > /tmp/timer_pid
-fi
+}
 
-if [ "$1" = "status" ]; then
-    sed -n '2p' /tmp/timer_log
-fi
+status(){
+    sed -n '1,2p' /tmp/timer_log
+}
 
-if [ "$1" = "finish" ]; then
-    echo "finalizado"
-    kill "$(cat /tmp/timer_pid)"
+finish(){
+    STATUS=$( cat /tmp/timer_log| awk 'NR==2' | awk '{print $2}' )
+    sed -i "2s/$STATUS/finalizado/" /tmp/timer_log
+    kill_process
+    sed -n '1,2p' /tmp/timer_log
     exit 0
-fi
+}
 
+case $1 in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    status)
+        status
+        ;;
+    continue)
+        Continue
+        ;;
+    finish)
+        finish
+        ;;
+esac
